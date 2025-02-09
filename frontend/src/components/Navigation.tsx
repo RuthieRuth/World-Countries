347a97c0b2fc4caaaa6aefaf85ce8c36ffc69eb5
Ruth <ruthasafuadjaye@yahoo.com>
2025-02-09 18:23:17 +0200

    Frontend: added authecation page/login through navigation, auth-family,LogIn, ProtectedRoute and supabase

---
+++ frontend/src/components/Navigation.tsx
@@ -0,0 +1,22 @@
+import { AppBar, Button, Toolbar } from '@mui/material';
+//import {useAuth} from '../context/auth-context';
+import {useAuth} from '../context/AuthContext';
+import {Link as RouterLink} from 'react-router-dom';
+
+export const Navigation = () => {
+    const {user, signOut} = useAuth();
+
+    return (
+
+        <AppBar position="static" sx={{mb:3}}>
+            <Toolbar>
+                <Button color="inherit" component={RouterLink} to="/">Home</Button>
+                <Button color="inherit" component={RouterLink} to="/test">Test</Button>
+                {user ? (<Button color="inherit" onClick={signOut}>Logout ({user.email})</Button>)
+                      : (<Button color="inherit" component={RouterLink} to="/login">Login</Button>)
+                }
+            </Toolbar>
+        </AppBar>
+
+    )
+};
