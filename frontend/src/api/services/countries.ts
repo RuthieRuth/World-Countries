21618993f46c9282cafd894d6bac0e26c0e73368
RuthieRuth <ruthasafuadjaye@yahoo.com>
2025-11-17 08:48:06 +0200

    Update to help deployment and rendering

--- frontend/src/api/services/countries.ts
+++ frontend/src/api/services/countries.ts
@@ -1,6 +1,24 @@
 import { Country } from "../../types/country";
 import { api } from "../axios";
 
+// Rest Countries recently changed how data is accessed; prefer requesting
+// only the fields we need via query params to reduce payload and avoid
+// relying on the old 'all' behaviour.
+const RESTCOUNTRIES_FIELDS = [
+    'name',
+    'capital',
+    'region',
+    'subregion',
+    'population',
+    'flags',
+    'cca3',
+    'currencies',
+    'latlng',
+    'maps',
+].join(',');
+
 export const countriesAPI = {
-    getAllCountries:  () : Promise<Country[]> => api.get('https://restcountries.com/v3.1/all'),
-}
+    // Returns a Promise resolving to the array of countries (response.data)
+    getAllCountries: (): Promise<Country[]> =>
+        api.get(`https://restcountries.com/v3.1/all?fields=${RESTCOUNTRIES_FIELDS}`),
+};
